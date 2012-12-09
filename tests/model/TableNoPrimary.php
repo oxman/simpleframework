0d75295f3c322115aa48eceeec0003528431310d
Sylvain <sylvain.robez-masson@skyboard.fr>
2012-12-10 00:04:46 +0100

    some tests models

---
+++ tests/model/TableNoPrimary.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * orm:name(T_TABLE_NO_PRIMARY_TNP)
+ */
+class TableNoPrimary extends \simpleframework\Norm\Model
+{
+
+
+    /**
+     * orm:type(int)
+     * orm:name(tnp_id)
+     */
+    protected $_id;
+
+
+}
